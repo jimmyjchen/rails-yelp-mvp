b32041a3afa11447a04ea013bf4c710bc9870579
Jimmy Chen <jimmyjchen@outlook.com>
2019-07-24 14:33:09 +0800

    Completed Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   root to: 'restaurants#index'
 
   resources :restaurants, only: [:index, :new, :add, :show]
+
+  resources :restaurants do
+    resources :reviews, only: [ :new, :create ]
+  end
+
+  get "restaurants/:id", to: "restaurants#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,25 @@
 
-puts "Attempting to 20 restaurants..."
+# puts "Attempting to create 20 restaurants..."
 
-20.times do
-  Restaurant.create!(
-    name: Faker::Hipster.word.capitalize,
-    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
-    phone_number: Faker::PhoneNumber.phone_number,
-    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+# 20.times do
+#   Restaurant.create!(
+#     name: Faker::Hipster.word.capitalize,
+#     address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+#     phone_number: Faker::PhoneNumber.phone_number,
+#     category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+#     )
+# end
+
+# puts "Successfully created #{Restaurant.count} restaurants."
+
+puts "Attempting to create 100 reviews..."
+
+100.times do
+  Review.create!(
+    content: Faker::Hipster.paragraphs,
+    rating: (1..5).to_a.sample,
+    restaurant_id: (21..40).to_a.sample
     )
 end
 
-puts "Successfully created #{Restaurant.count} restaurants."
+puts "Successfully created #{Review.count} reviews."
